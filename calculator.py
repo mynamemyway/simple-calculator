ae84fe6038f218991cd10d201c8ff06048d9e697
Aleksandr Samokhvalov <samokhvaloff.on@gmail.com>
2025-04-30 16:39:35 +0300

    1.0

---
+++ calculator.py
@@ -0,0 +1,14 @@
+num_1, num_2, op = int(input()), int(input()), input()
+if op == "+":
+    print(num_1 + num_2)
+elif op == "-":
+    print(num_1 - num_2)
+elif op == "*":
+    print(num_1 * num_2)
+elif op == "/":
+    if num_2 != 0:
+        print(num_1 / num_2)
+    else:
+        print("На ноль делить нельзя")
+else:
+    print("Неверная операция")
